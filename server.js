b3dac3e8c184a319b14250d84c6df03a2f47650c
Monkisoul <jalegc@hotmail.com>
2021-06-24 22:46:38 -0500

    server fix for react

--- server.js
+++ server.js
@@ -1,24 +1,38 @@
+const cors = require('cors')
 const express = require('express');
 const app = express();
 const { Deck, Hand } = require('./public/deck.js');
 
-app.use('/', express.static(`${__dirname}/public`))
+
+app.use(cors())
+app.use(express.static(`${__dirname}/public`))
 app.use(express.json());
 
 const deck = new Deck()
 const table = new Hand(deck, +5)
 
+app.get('/table', (req, res)=> {
+   res.json({ hand: table.cards })
+})
+  
+app.get('/hand', (req, res) =>{
+  const hand = new Hand(deck, +2)
+   res.json({
+    hand: hand.cards,
+    
+  })
+})  
 app.get('/fetching', (req, res) => {
   const hands = [];
   hands.push(new Hand(deck, +2))
-  return res.json({
+   res.json({
     hands: hands.map((hand) => hand.cards),
     deck: table.cards
   })
 })
 
 
-app.listen(4001, () => {
-  console.log('Server running on port 4001');
+app.listen(4040, () => {
+  console.log('Server running on port 4040');
 });
 
